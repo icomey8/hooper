368e01436330a0591e420badf985f3ce4e983bd6
icomey8 <i.willcomey@gmail.com>
2025-01-29 14:40:36 -0800

    set up ohm lib

--- src/hooper.js
+++ src/hooper.js
@@ -1,1 +1,13 @@
-console.log("hello from hooper.js!");
+import * as fs from "fs";
+import * as ohm from "ohm-js";
+
+const grammar = ohm.grammar(fs.readFileSync("src/hooper.ohm", "utf-8"));
+const sourceCode = process.argv[2];
+const match = grammar.match(sourceCode);
+
+if (match.succeeded()) {
+	console.log("this program is syntactically correct");
+} else {
+	console.error("this program is syntactically incorrect");
+	console.error(match.message);
+}

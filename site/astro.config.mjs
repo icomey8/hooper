f1e643a2d985df0e54c0109b67601c0f98fd8ddf
icomey8 <i.willcomey@gmail.com>
2025-03-18 11:14:44 -0700

    new site with new workflow file

---
+++ site/astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+	site: "https://icomey8.github.io",
+	base: "/hooper/",
+	vite: {
+		plugins: [tailwindcss()],
+	},
+});

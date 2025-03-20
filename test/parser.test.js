4ef42f858f3697f5e140360679c413521622f63a
icomey8 <i.willcomey@gmail.com>
2025-03-19 22:39:39 -0700

    fixed a couple of parser tests

--- test/parser.test.js
+++ test/parser.test.js
@@ -5,16 +5,16 @@ import parse from "../src/parser.js";
 const syntaxChecks = [
 	["simplest syntactically correct program", "break;"],
 	["arithmetic", "log 2 * x + 3 / 5 - -1 % 7 ** 3 ** 3;"],
-	["variable declarations", "let e=99*1;\nlet z=false;"],
+	["variable declarations", "pick e=99*1;\npick z=false;"],
 	["assignments", "abc=9*3; a=1; a--; c++;"],
 	["this is an valid comment", "// i am a hooper comment!"],
 ];
 
 const syntaxErrors = [
-	["malformed number", "let x= 2.;", /Line 1, col 10:/],
-	["a float with an E but no exponent", "let x = 5E * 11;", /Line 1, col 10:/],
-	["a missing right operand", "log(5 -);", /Line 1, col 10:/],
-	["assignments", "abc=9*3; a=1; a--; c++;"], // change this to be invalid
+	["malformed number", "pick x= 2.;", /Line 1, col 11:/],
+	["a float with an E but no exponent", "pick x = 5E * 11;", /Line 1, col 12:/],
+	["a missing right operand", "log(5 -);", /Line 1, col 8:/],
+	["assignments", "abc=9*3; a=1; a--; c++;"], // this needs to be invalid
 	["this is an invalid comment", "# i am a python comment!;", /Line 1, col 1:/],
 ];
 

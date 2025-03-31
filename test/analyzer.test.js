bd0f7bf7c9c5994b93f6c718076476b616340c62
icomey8 <i.willcomey@gmail.com>
2025-03-30 22:54:27 -0700

    added some more analyzer tests

--- test/analyzer.test.js
+++ test/analyzer.test.js
@@ -11,6 +11,16 @@ const semanticChecks = [
 	["arithmetic expressions", "pick x = 5 + 3 * 2;"],
 	["array declaration", "pick arr = [1, 2, 3];"],
 	["conditional", "if true { log 42; }"],
+	["increment", "pick x = 10; x++;"],
+	["assign arrays", "pick a: number[] = [2, 3]; pick b=[1]; a=b ;b=a;"],
+	["assign to array element", "pick a = [1,2,3]; a[1]=100;"],
+	["or", "log(true or 1<2 or false);"],
+	["and", "log(true and 1<2 and false);"],
+	["ok to == arrays", "log([2]==[8]);"],
+	["ok to != arrays", "log([3]!=[24]);"],
+	["arithmetic", "pick x = 1; log(2 * 3 + 5 ** 5);"],
+	["subscript exp", "pick a = [1, 2]; log(a[0]);"],
+	["array parameters", "define play f(x: number[]) {}"],
 ];
 
 // Programs that are syntactically correct but have semantic errors

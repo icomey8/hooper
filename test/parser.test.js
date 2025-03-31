236f44496ed6d2e6269949173b3f385f469d0890
icomey8 <i.willcomey@gmail.com>
2025-03-30 22:25:56 -0700

    added some more parser tests

--- test/parser.test.js
+++ test/parser.test.js
@@ -2,19 +2,40 @@ import { describe, it } from "node:test";
 import assert from "node:assert/strict";
 import parse from "../src/parser.js";
 
-// ADD MORE PARSER TESTS
-
 const syntaxChecks = [
 	["simplest syntactically correct program", "break;"],
 	["arithmetic", "log 2 * x + 3 / 5 - -1 % 7 ** 3 ** 3;"],
-	["variable declarations", "pick e=99*1;\npick z=false;"],
+	["variable declarations", "pick e = 99*1;\npick z = false;"],
+	[
+		"variable declarations with types",
+		"pick e: number = 99*1;\npick z: boolean = false;",
+	],
 	["assignments", "abc=9*3; a=1; c++;"],
 	["this is an valid comment", "# i am a hooper comment!"],
 	["short if", "if x == 2 { log(2); }"],
+	[
+		"if with an elseif statement",
+		"if x == 2 { log(2); } putback if x != 2 { log(3);} ",
+	],
 	["if with an else statement", "if x == 2 { log(2); } putback { log(3);} "],
-	["short if statement", "if x == 2 { log(2); }"],
-	["assignment to array element", "c[2] = 100;"], // add to other
-	["array element increment", "c[2]++;"], // add to other
+	["assignment to array element", "c[2] = 100;"],
+	["array element increment", "c[2]++;"],
+	["function with no params, no return type", "define play f() {}"],
+	[
+		"function with typed params",
+		"define play add_nums(x: number, y: boolean) {}",
+	],
+	[
+		"function with return type",
+		"define play add_nums(x: number, y: boolean) -> number {}",
+	],
+	["array type for param", "define play arrays(x: Arr[boolean]) {}"],
+	["array type returned", "define play f() -> Arr[string] {}"],
+	["call in exp", "log(5 * f(x, y, 2 * y));"],
+	["while with empty block", "while true {}"],
+	["while with one statement block", "while true { pick x = 1; }"],
+	["ors can be chained", "log(2 or 8 or 90);"],
+	["ands can be chained", "log(1 and 2 and 6);"],
 ];
 
 const syntaxErrors = [
@@ -25,9 +46,25 @@ const syntaxErrors = [
 	["this is an invalid comment", "// i am a js comment!;", /Line 1, col 1:/],
 	["invalid short if", "if { log(2); }", /Line 1, col 4:/],
 	["if as identifier", "if x = 3;", /Line 1, col 6:/],
-	["booleans are not incrementable", "true--;", /Line 1, col 5:/], // add to other
-	["booleans do not have subscripts", "true[];", /Line 1, col 5:/], // add to other
-	["true is not assignable", "true = 1;", /Line 1, col 5/], // add to other
+	["booleans are not incrementable", "true--;", /Line 1, col 5:/],
+	["booleans do not have subscripts", "true[];", /Line 1, col 5:/],
+	["true is not assignable", "true = 1;", /Line 1, col 5/],
+	["array subscript with no index", "c[] = 100;", /Line 1, col 3:/],
+	[
+		"incorrect array type for param",
+		"define play arrays(x: [boolean]) {}",
+		/Line 1, col 23:/,
+	],
+	[
+		"incorrect array return structure",
+		"define play f(): Arr[string] {}",
+		/Line 1, col 16:/,
+	],
+	["mixing ands and ors", "log('dog' and 'cat or 'mouse);", /Line 1, col 5:/],
+	["unbalanced brackets", "define play x(): boolean[;", /Line 1, col 16/],
+	["empty subscript", "log(a[]);", /Line 1, col 7/],
+	["numbers cannot be subscripted", "log(143[x]);", /Line 1, col 8/],
+	["numbers cannot be called", "log(20(a));", /Line 1, col 7/],
 ];
 
 describe("The parser", () => {

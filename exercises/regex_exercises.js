6a30a2b88f750db0c239b7022126a70e15497c8f
icomey8 <i.willcomey@gmail.com>
2025-05-03 16:31:58 -0700

    completed some regexs

---
+++ exercises/regex_exercises.js
@@ -0,0 +1,18 @@
+const regexes = {
+	canadianPostalCode:
+		/^[A-CEGHJ-NPR-TV-Z]\d[A-CEGHJ-NPR-TV-Z] \d[A-CEGHJ-NPR-TV-Z]\d$/,
+	visa: /^4(?:\d{12}|\d{15})$/,
+	masterCard:
+		/^(?:5[1-5]\d{14})|(?:222[1-9]|22[3-9]\d|2[3-6]\d{2}|27[01]\d|2720)\d{12}$/,
+	notThreeEndingInOO: /^(?![A-Za-z][oO]{2}$)[A-Za-z]*$/,
+	divisibleBy16: /^$/,
+	eightThroughThirtyTwo: /^(?:[89]|[1-2]\d|3[0-2])$/,
+	notPythonPycharmPyc: /^(?!(?:python|pycharm|pyc)$)\p{L}*$/u,
+	restrictedFloats: /^ ........ $/i,
+	palindromes2358: /^ ........ $/,
+	pythonStringLiterals: /^ ........ $/,
+};
+
+export function matches(name, string) {
+	return regexes[name].test(string);
+}
